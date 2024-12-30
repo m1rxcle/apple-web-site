ed146f35317206f703e0dc2caffd58d52f106f0e
Denis M1rxcle <noonebesideu@gmail.com>
2024-12-30 20:57:37 +0200

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Features from "./components/features"
+import Footer from "./components/footer"
+import Hero from "./components/hero"
+import Highlights from "./components/highlights"
+import HowItWorks from "./components/how-it-works"
+import Model from "./components/model"
+import Navbar from "./components/navbar"
+import * as Sentry from "@sentry/react"
+function App() {
+	return (
+		<main className="bg-black">
+			<Navbar />
+			<Hero />
+			<Highlights />
+			<Model />
+			<Features />
+			<HowItWorks />
+			<Footer />
+		</main>
+	)
+}
+
+export default Sentry.withProfiler(App)
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React, { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+
+import * as Sentry from "@sentry/react"
+
+Sentry.init({
+	dsn: "https://62b785d4a828e9c38f1c77e90c8fb189@o4508549173149696.ingest.de.sentry.io/4508549175902288",
+	integrations: [
+		Sentry.browserTracingIntegration(),
+		Sentry.reactRouterV6BrowserTracingIntegration({
+			useEffect: React.useEffect,
+		}),
+		Sentry.replayIntegration(),
+	],
+	tracesSampleRate: 1.0,
+	tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0,
+})
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+)
