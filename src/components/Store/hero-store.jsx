e9e81e91369edd1022b1803c8ff8b63ba062c58a
Denis M1rxcle <noonebesideu@gmail.com>
2025-02-01 11:41:03 +0200

    Store hero animations

--- src/components/Store/hero-store.jsx
+++ src/components/Store/hero-store.jsx
@@ -4,15 +4,18 @@ import ProductsList from "./products-list"
 
 const HeroStore = () => {
 	useGSAP(() => {
-		gsap.to(".hero-title", { opacity: 1, y: 0, delay: 0.5 })
+		gsap.to(".hero-title", { opacity: 1, delay: 0.5, x: 0 })
+		gsap.to(".hero-pretitle", { opacity: 1, delay: 0.5, x: 0 })
+		gsap.to("#products", { opacity: 1, delay: 1, ease: "power1.inOut" })
+		gsap.from("#products", { scale: 0.3, delay: 1.5, ease: "power1.inOut" })
 	}, [])
 	return (
 		<section className="w-full nav-height bg-black relative common-padding ">
-			<div className="flex items-center gap-2 mb-24 screen-max-width ">
-				<h1 className="hero-title">Store.</h1>
-				<p className="text-3xl">The best way to buy the products you love.</p>
+			<div className="flex justify-center items-center gap-1 mb-24 screen-max-width">
+				<h1 className="hero-title -translate-x-10">Store.</h1>
+				<p className="hero-pretitle text-3xl translate-x-10 opacity-0">The best way to buy the products you love.</p>
 			</div>
-			<div className="flex justify-around items-center overflow-hidden">
+			<div id="products" className="opacity-0 flex justify-around items-center overflow-hidden">
 				<ProductsList />
 			</div>
 		</section>
