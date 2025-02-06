79774972f415391642e531d8d0f21be7cc7d68c1
Denis M1rxcle <noonebesideu@gmail.com>
2025-02-06 05:05:28 +0200

    fixed assets

--- src/utils/index.js
+++ src/utils/index.js
@@ -72,16 +72,16 @@ export const explore2Img = explore2
 export const chipImg = chip
 export const frameImg = frame
 
-import performanceMac from "./../../assets/videos/performance-mac.mp4"
+import performanceMac from "/assets/videos/performance-mac.mp4"
 export const performanceMacVideo = performanceMac
 
-import macFeatures from "./../../assets/images/mac-features.jpg"
-import photoForFeatures1 from "./../../assets/images/photo-for-features1.png"
-import photoForFeatures2 from "./../../assets/images/photo-for-features2.png"
-import photoForFeatures3 from "./../../assets/images/photo-for-features3.png"
-import photoForFeatures4 from "./../../assets/images/photo-for-features4.png"
-import photoForFeatures5 from "./../../assets/images/photo-for-features5.png"
-import photoForFeatures6 from "./../../assets/images/photo-for-features6.png"
+import macFeatures from "/assets/images/mac-features.jpg"
+import photoForFeatures1 from "/assets/images/photo-for-features1.png"
+import photoForFeatures2 from "/assets/images/photo-for-features2.png"
+import photoForFeatures3 from "/assets/images/photo-for-features3.png"
+import photoForFeatures4 from "/assets/images/photo-for-features4.png"
+import photoForFeatures5 from "/assets/images/photo-for-features5.png"
+import photoForFeatures6 from "/assets/images/photo-for-features6.png"
 
 export const macFeaturesImg = macFeatures
 export const photoForFeatures1Img = photoForFeatures1
@@ -91,34 +91,34 @@ export const photoForFeatures4Img = photoForFeatures4
 export const photoForFeatures5Img = photoForFeatures5
 export const photoForFeatures6Img = photoForFeatures6
 
-import hightLightForMacVideoFirst from "./../../assets/videos/hightlight-for-mac-video1.mp4"
-import hightLightForMacVideoSecond from "./../../assets/videos/hightlight-for-mac-video2.mp4"
-import hightLightForMacVideoFour from "./../../assets/videos/highlights-for-mac-video4.mp4"
-import hightLightForMacVideoThird from "./../../assets/videos/hightlight-for-mac-video3.mp4"
+import hightLightForMacVideoFirst from "/assets/videos/hightlight-for-mac-video1.mp4"
+import hightLightForMacVideoSecond from "/assets/videos/hightlight-for-mac-video2.mp4"
+import hightLightForMacVideoFour from "/assets/videos/highlights-for-mac-video4.mp4"
+import hightLightForMacVideoThird from "/assets/videos/hightlight-for-mac-video3.mp4"
 
 export const hightLightForMacVideoFirstVideo = hightLightForMacVideoFirst
 export const hightLightForMacVideoSecondVideo = hightLightForMacVideoSecond
 export const hightLightForMacVideoThirdVideo = hightLightForMacVideoThird
 export const hightLightForMacVideoFourthVideo = hightLightForMacVideoFour
 
-import macBookTitleImg from "./../../assets/images/mac-book-title-img.png"
-import macBookLarge from "./../../assets/videos/macBookLarge.mp4"
-import macBookSmall from "./../../assets/videos/macBookSmall.mp4"
+import macBookTitleImg from "/assets/images/mac-book-title-img.png"
+import macBookLarge from "/assets/videos/macBookLarge.mp4"
+import macBookSmall from "/assets/videos/macBookSmall.mp4"
 
 export const macBookTitleImgSrc = macBookTitleImg
 export const macBookLargeSrc = macBookLarge
 export const macBookSmallSrc = macBookSmall
 
-import iPhoneCurImg from "./../../assets/images/iPhone-card.jpeg"
-import iPhoneCurImg2 from "./../../assets/images/iPhone-card-2.jpeg"
-import iPadCurImg from "./../../assets/images/ipad-card.jpeg"
-import iPadCurImg2 from "./../../assets/images/ipad-card-2.jpeg"
-import iPadProCurImg from "./../../assets/images/ipadPro-card.jpeg"
-import macCurImg from "./../../assets/images/mac-card.jpeg"
-import macMiniCurImg from "./../../assets/images/macMini-card.jpeg"
-import watchCurImg from "./../../assets/images/watch-card.jpeg"
-import watchCurImg2 from "./../../assets/images/watch-card-2.jpeg"
-import watchCurImg3 from "./../../assets/images/watch-card-3.jpeg"
+import iPhoneCurImg from "/assets/images/iPhone-card.jpeg"
+import iPhoneCurImg2 from "/assets/images/iPhone-card-2.jpeg"
+import iPadCurImg from "/assets/images/ipad-card.jpeg"
+import iPadCurImg2 from "/assets/images/ipad-card-2.jpeg"
+import iPadProCurImg from "/assets/images/ipadPro-card.jpeg"
+import macCurImg from "/assets/images/mac-card.jpeg"
+import macMiniCurImg from "/assets/images/macMini-card.jpeg"
+import watchCurImg from "/assets/images/watch-card.jpeg"
+import watchCurImg2 from "/assets/images/watch-card-2.jpeg"
+import watchCurImg3 from "/assets/images/watch-card-3.jpeg"
 
 export const iPhoneCurImgSrc = iPhoneCurImg
 export const iPhoneCurImg2Src = iPhoneCurImg2
@@ -131,8 +131,8 @@ export const watchCurImgSrc = watchCurImg
 export const watchCurImg2Src = watchCurImg2
 export const watchCurImg3Src = watchCurImg3
 
-import entImgBig from "./../../assets/images/ent-img.png"
-import entImgSmall from "./../../assets/images/ent-img-sm.jpg"
+import entImgBig from "/assets/images/ent-img.png"
+import entImgSmall from "/assets/images/ent-img-sm.jpg"
 
 export const entImgBigSrc = entImgBig
 export const entImgSmallSrc = entImgSmall
